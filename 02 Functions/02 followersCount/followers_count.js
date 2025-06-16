daee3a7122e85bccfd43877ccb74e97ee6a6eb0f
ibrahimkewan0 <ibrahimkewan0@gmail.com>
2025-06-16 07:05:04 -0400

    add reset function

--- 02 Functions/02 followersCount/followers_count.js
+++ 02 Functions/02 followersCount/followers_count.js
@@ -8,9 +8,16 @@ function displayCount(){
     document.getElementById("countDisplay").innerHTML = count;
 }
 function checkCountValue(){
-    if(count == 10){
+    if (count == 0){
+        alert("Follower-Zählung wurde zurückgesetzt ...");
+    }else if(count == 10){
         alert("Ihr Instagram-Beitrag hat 10 follower gewonnen! Herzlichen Glückwunsch!");
     }else if(count == 20){
         alert("Ihr Instagram-Beitrag hat 20 Follower gewonnen! Machen Sie weiter so!");
     }
+}
+function reset(){
+    count = 0;
+    displayCount();
+    checkCountValue();
 }
