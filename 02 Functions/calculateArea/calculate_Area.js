a4ad25f2ccfab62d46aba1a96cd89db255b6c48a
ibrahimKewan0 <ibrahimKewan0@gmail.com>
2025-06-16 06:01:41 -0400

    adding sum the amount function

--- 02 Functions/calculateArea/calculate_Area.js
+++ 02 Functions/calculateArea/calculate_Area.js
@@ -1,10 +1,18 @@
-let length;
-let width;
-
 function calculateArea(){
+    let length;
+    let width;
+
     length = parseFloat(document.getElementById("length").value);
     width = parseFloat(document.getElementById("width").value);
 
     let area = length * width;
     document.getElementById("result").innerText = `The area of rechEck ist: ${area}`;
+}
+
+function calculateGrocery(){
+    let gercery_1 = parseFloat(document.getElementById("1_Lebensmittel").value);
+    let gercery_2 = parseFloat(document.getElementById("2_Lebensmittel").value);
+    let gercery_3 = parseFloat(document.getElementById("3_Lebensmittel").value);
+    let sum =  gercery_1 + gercery_2 + gercery_3;
+    document.getElementById("result_Grocery").innerText = `Die gesamte kauf ist: ${sum}`;
 }
