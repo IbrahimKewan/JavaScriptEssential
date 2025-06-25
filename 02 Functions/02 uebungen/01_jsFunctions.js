8c035003506748a2adb3cc19ee7571160e9c3f11
ibrahimkewan0 <ibrahimkewan@outlook.de>
2025-06-25 10:17:27 +0200

    weitere funktonerne uhrzeit setTimeout funktionen

--- 02 Functions/02 uebungen/01_jsFunctions.js
+++ 02 Functions/02 uebungen/01_jsFunctions.js
@@ -82,3 +82,39 @@ meinZaeler();
 meinZaeler();
 const meinZaeler_2222 = zae();
 meinZaeler_2222();
+
+// Callback mit Verzögerung Erstelle eine Funktion warteUndRufe(callback, sekunden), die nach sekunden den Callback aufruft (verwende setTimeout).
+function warteUndRufe(callback, sekunden){
+    setTimeout(callback,sekunden * 1000);
+}
+function hallo() {
+    console.log("Hallo nach 3 Sekunden!");
+}
+warteUndRufe(hallo, 3);
+
+// Akruelle uhrzeit 
+const lblTime = document.getElementById("zeit");
+function zeit(){
+    let jetzt = new Date();
+    let st = jetzt.getHours();
+    let min = jetzt.getMinutes();
+    let sek = jetzt.getSeconds();
+    function pad(zahl){
+        if(zahl <10){
+            return `0${zahl}`;
+        }
+        return zahl;
+    }
+    lblTime.innerHTML = `${pad(st)}:${pad(min)}:${pad(sek)}`;
+    setTimeout(zeit, 1000);
+}
+zeit();
+
+// Button zähler
+const BtnZaehler = document.getElementById("zaehler");
+let x = 0;
+function zaehler(){
+    x++;
+    console.log(`btn Zähler: ${x}`);
+}
+BtnZaehler.addEventListener("click", zaehler);
