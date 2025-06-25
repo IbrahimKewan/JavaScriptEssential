04c6d6728fe42b8e15d0852faee9ae4b8dd691f0
ibrahimkewan0 <ibrahimkewan@outlook.de>
2025-06-25 12:21:10 +0200

    level 3 aufgaben

---
+++ 02 Functions/02 uebungen/level 3/script.js
@@ -0,0 +1,35 @@
+// Level 3 – DOM & Eventhandling
+//     Klickzähler mit HTML HTML: Ein Button mit id="klickButton" Aufgabe: Zähle, wie oft er geklickt wurde – und gib die Zahl im Button-Text aus.
+const zaehler = document.getElementById("zaehler");
+let x = 0;
+zaehler.addEventListener("click", function(){
+    x++;
+    document.getElementById("clickANZ").innerText = `click ANZ: ${x}`;
+});
+const resetCounter = document.getElementById("resetCounter");
+resetCounter.addEventListener("click", function(){
+    x = 0;
+    document.getElementById("clickANZ").innerText = `click ANZ: ${x}`;
+});
+
+//     Mehrere Event-Handler Füge zwei verschiedene click-Event-Listener an denselben Button an. Jeder soll etwas anderes tun.
+const btnMoreClicks = document.getElementById("btnMoreClicks");
+const lblMoreClicks = document.getElementById("lblMoreClicks")
+btnMoreClicks.addEventListener("click", function(){
+    document.getElementById("clickANZ").style.color = "red";
+    lblMoreClicks.innerHTML = "One Click ...";
+    lblMoreClicks.style.color = "red";
+});
+btnMoreClicks.addEventListener("dblclick", function(){
+    document.getElementById("clickANZ").style.color = "green";
+    lblMoreClicks.innerHTML = "dblClick ...";
+    lblMoreClicks.style.color = "green";
+});
+
+//     Interaktive Eingabe HTML: Textfeld + Button JS: Beim Klick soll der Inhalt des Textfelds ausgelesen und in einem <div> angezeigt werden.
+
+// reload
+const btnReload = document.getElementById("btnReload");
+btnReload.addEventListener("click", function(){
+    location.reload();
+})
