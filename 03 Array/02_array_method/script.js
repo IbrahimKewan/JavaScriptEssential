1e5dfece8d5788da731805866f1f2fdfecc4adba
ibrahimkewan0 <ibrahimkewan@outlook.de>
2025-07-01 16:24:26 +0200

    array method map reduce

--- 03 Array/02_array_method/script.js
+++ 03 Array/02_array_method/script.js
@@ -2,5 +2,17 @@ const employees = [
       { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
       { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
       { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-      //... Weitere Mitarbeiterdatensätze können hier hinzugefügt werden
     ];
+function displayEmployees(){
+  const displayEmployees = employees.map((employ,index) => 
+    `<p>${employ.id}: ${employ.name} - ${employ.age} - ${employ.department} - ${employ.salary}</p>`
+  ).join('');
+  document.getElementById('employeesDetails').innerHTML = displayEmployees;
+}
+
+function calculateTotalSalaries(){
+  const TotalSalaries = employees.reduce((speicher,aktElem) => {
+    return speicher + aktElem.salary;
+  },0);
+  document.getElementById('employeesDetails').innerHTML = `<strong>Die gesamte gehalte:</strong> <mark>${TotalSalaries}</mark>`;
+}
