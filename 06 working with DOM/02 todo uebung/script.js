c70e4bb048eeb5d8fd08bef2467b0351a4819597
ibrahimkewan0 <ibrahimkewan@outlook.de>
2025-07-15 11:12:58 +0200

    add todo list js training

--- 06 working with DOM/02 todo uebung/script.js
+++ 06 working with DOM/02 todo uebung/script.js
@@ -1,28 +1,46 @@
+const formular = document.getElementById('formular');
 const btnAdd = document.getElementById('add');
 const btnDelete = document.getElementById('delete');
-let formular = document.getElementById('formular');
-let arrInputs = [];
+let liste = [];
 
 btnAdd.addEventListener('click', () =>{
     const input = document.getElementById('input').value;
-    if(!input || input.trim() === ""){
-        window.alert('Bitte textfeld eingeben!!!');
+    if (!input || input.trim() === ""){
+        window.alert('textbox darf nicht leer sein');
         return;
     }
-    arrInputs.push(input);
+    liste.push({text: input, done:false});
     displayList();
 });
 
 function displayList(){
     formular.innerHTML = '';
-    arrInputs.forEach((item,index) =>{
+    liste.forEach((item,index) =>{
         const div = document.createElement('div');
-        div.textContent = `${index+1}: ${item}`;
+        const checkbox = document.createElement('input');
+        const label = document.createElement('label');
+        checkbox.id = `cbListe_${index}`;
+        checkbox.type = 'checkbox';
+        label.textContent = item.text;
+
+        checkbox.checked = item.done;
+        if (item.done){
+            label.classList.add('done');
+        }
+
+        checkbox.addEventListener('change', () => {
+            item.done = checkbox.checked;
+            label.classList.toggle('done', item.done);
+        });
+
+        div.appendChild(checkbox);
+        div.appendChild(label);
         formular.appendChild(div);
     });
 }
-btnDelete.addEventListener('click', () =>{
-    arrInputs = [];
+
+btnDelete.addEventListener('click', (item, index) =>{
+    liste = [];
     displayList();
     document.getElementById('input').value = '';
 });
